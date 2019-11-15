c2f13299c4031246edc721c83f9c5fb36a226934
Adam Johnson <me@adamj.eu>
2019-11-15 10:10:49 +0000

    Support Python 3.8 (#65)

--- requirements/compile.sh
+++ requirements/compile.sh
@@ -6,3 +6,4 @@ python3.4 -m piptools compile --generate-hashes "$@" -o py34.txt
 python3.5 -m piptools compile --generate-hashes "$@" -o py35.txt
 python3.6 -m piptools compile --generate-hashes "$@" -o py36.txt
 python3.7 -m piptools compile --generate-hashes "$@" -o py37.txt
+python3.8 -m piptools compile --generate-hashes "$@" -o py38.txt
